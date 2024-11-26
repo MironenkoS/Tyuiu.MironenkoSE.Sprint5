5af67a3acc574a1a44440cd0d4ee5104b9854fcb
664 <stephencurrytopone@gmail.com>
2024-11-26 20:01:49 +0500

    Task 3

--- Tyuiu.MironenkoSE.Sprint5.Task3.V17.Lib/DataService.cs
+++ Tyuiu.MironenkoSE.Sprint5.Task3.V17.Lib/DataService.cs
@@ -8,29 +8,15 @@ namespace Tyuiu.MironenkoSE.Sprint5.Task3.V17.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            double calculatedResult = 2.4 * Math.Pow(x, 3) + 0.4 * Math.Pow(x, 2) - 1.4 * x + 4.1; 
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
 
-
-            int intResult = (int)Math.Round(calculatedResult);
-
-
-            string tempPath = Path.GetTempPath();
-
-
-            string filePath = Path.Combine(tempPath, "OutPutFileTask3.bin");
-            string tx = "MzMzMzMTUUA=";
-
-            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Append)))
             {
-                writer.Write(intResult);
+                writer.Write(Math.Round(2.4 * Math.Pow(x, 3) + 0.4 * Math.Pow(x, 2) - 1.4 * x + 4.1, 3));
+
             }
 
-
-            byte[] fileBytes = File.ReadAllBytes(filePath);
-            string base64String = Convert.ToBase64String(fileBytes);
-
-
-            return filePath;
+            return path;
         }
     }
 }
