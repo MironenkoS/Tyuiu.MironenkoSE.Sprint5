f578efa2b416471e6119a44e1403cf9edb15ece9
664 <stephencurrytopone@gmail.com>
2024-11-26 17:35:54 +0500

    Task 0

--- Tyuiu.MironenkoSE.Sprint5.Task0.V13.Lib/DataService.cs
+++ Tyuiu.MironenkoSE.Sprint5.Task0.V13.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.MironenkoSE.Sprint5.Task0.V13.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
-            double res =1;
+            double res = (-1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x);
             res = Math.Round(res, 3);
             File.WriteAllText(path, Convert.ToString(res));
             return path;
